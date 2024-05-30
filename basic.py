ecc454e1f6a61d669e3069cd72c589e0c409a2a4
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-05-30 23:06:26 +0300

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,30 @@
+print("1 task")
+a = "Kol"
+print(len(a))
+
+print("2 task")
+first = 4
+second = 6
+summa = first + second
+dif = first - second
+print(summa)
+print(dif)
+
+print("3 task")
+#Используем данные из 2 задачи
+third = 2
+mean = (first+second+third)
+print(mean)
+
+print("4 task")
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string+","+" "+first_string)
+
+print("5 task")
+a = 4
+b = 3gj
+c = 8
+f = (a * b) + (a * c)
+print(f**3/2)
+
