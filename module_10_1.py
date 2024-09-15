e1d0ad7216c53c838c62db9c85384f720b764020
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-09-15 15:57:20 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from threading import Thread
+from time import sleep
+from datetime import datetime
+
+time_start = datetime.now()
+def wite_words(word_count, file_name):
+    for i in range(1, word_count):
+        with open(file_name,'a') as file:
+            file.write(f"Какое-то слово № {i}/n")
+    sleep(0.1)
+    time_end = datetime.now()
+    time_res = time_end - time_start
+    print(time_res)
+    return 'Завершилась запись в файл {file_name}'
+
+wite_words(10, "example1.txt")
+wite_words(30, "example2.txt")
+wite_words(200, "example3.txt")
+wite_words(100, "example4.txt")
+
+thr_first = Thread(target=wite_words, args=(10, "example5.txt", ))
+thr_second = Thread(target=wite_words, args=(30, "example6.txt", ))
+thr_third = Thread(target=wite_words, args=(200, "example7.txt", ))
+thr_fourth = Thread(target=wite_words, args=(300, "example8.txt", ))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourth.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourth.join()
+
+time_end = datetime.now()
+time_res = time_end - time_start
+print(time_res)
