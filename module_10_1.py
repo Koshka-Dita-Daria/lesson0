fb0882dbbf07fed463a2074f42ed835955704231
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-09-15 16:10:49 +0300

    Update module_10_1.py

--- module_10_1.py
+++ module_10_1.py
@@ -8,17 +8,16 @@ def wite_words(word_count, file_name):
     with open(file_name,'w') as file:
         for i in range(1, word_count+1):
             file.write(f"Какое-то слово № {i}/n")
-    sleep(0.1)
-    time_end = datetime.now()
-    time_res = time_end - time_start
-    print(time_res)
+            sleep(0.1)
     return 'Завершилась запись в файл {file_name}'
 
 wite_words(10, "example1.txt")
 wite_words(30, "example2.txt")
 wite_words(200, "example3.txt")
 wite_words(100, "example4.txt")
-
+time_end = datetime.now()
+time_res = time_end - time_start
+print(time_res)
 thr_first = Thread(target=wite_words, args=(10, "example5.txt", ))
 thr_second = Thread(target=wite_words, args=(30, "example6.txt", ))
 thr_third = Thread(target=wite_words, args=(200, "example7.txt", ))
