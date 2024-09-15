673cf9af6e97f8954d55ae118d58edd16d59d35d
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-09-15 14:33:15 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+def is_prime(func):
+    def wrapper(d, e, f):
+        res = func(d, e, f)
+        k = 0
+        if res == 2 or res == 1:
+            print("Простое")
+        else:
+            for i in range(2, int(res/2)):
+                if res % i == 0:
+                    k += 1
+            if k == 0:
+                print("Простое")
+            else:
+                print("Составное")
+        return res
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a+b+c
+result = sum_three(2, 3, 6)
+print(result)
