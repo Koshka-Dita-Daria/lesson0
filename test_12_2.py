da46b7a0ead9deb93ceda9e66cc71dcc82e588ef
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-10-15 15:50:27 +0300

    Add files via upload

--- test_12_2.py
+++ test_12_2.py
@@ -1,7 +1,26 @@
 
 import unittest
-from Runner_Tournament import Runner, Tournament
 
+import Runner_Tournament
+from Runner_Tournament import Runner, Tournament
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        r1 = Runner('1')
+        for i in range(10):
+            r1.walk()
+        self.assertEqual(r1.distance, 50)
+    def test_run(self):
+        r1 = Runner('2')
+        for i in range(10):
+            r1.run()
+        self.assertEqual(r1.distance, 100)
+    def test_challenge(self):
+        r1 = Runner('3')
+        r2 = Runner('4')
+        for i in range(10):
+            r1.walk()
+            r2.run()
+        self.assertNotEqual(r1.distance, r2.distance)
 class TournamentTest(unittest.TestCase):
     def setUp(self):
         self.r1 = Runner("Усэйн", 10)
@@ -12,19 +31,23 @@ class TournamentTest(unittest.TestCase):
         cls.all_result = []
     @classmethod
     def tearDownClass(cls):
+        c = {}
         for i, el in enumerate(cls.all_result):
-            print(f"{i}: {el}")
-    def runners1(self):
+            for j in range(len(el)):
+                k = Runner.__str__(el[j+1])
+                c.update({j+1: k})
+            print(c)
+    def test_runners1(self):
         l1 = Tournament(90, self.r1, self.r3)
         res1 = l1.start()
         TournamentTest.all_result.append(res1)
         self.assertTrue(res1[2] == "Ник")
-    def runners2(self):
+    def test_runners2(self):
         l2 = Tournament(90, self.r2, self.r3)
         res2 = l2.start()
         TournamentTest.all_result.append(res2)
         self.assertTrue(res2[2] == "Ник")
-    def runners3(self):
+    def test_runners3(self):
         l3 = Tournament(90, self.r1, self.r2, self.r3)
         res3 = l3.start()
         TournamentTest.all_result.append(res3)
