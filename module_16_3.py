166a888d107beb7be035bfedbe77fd804c66d65e
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-10-28 18:26:27 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -14,7 +14,7 @@ async def call_dictionary() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def registration(username: str, age: int) -> str:
+async def registration(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")], age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
@@ -23,9 +23,10 @@ async def registration(username: str, age: int) -> str:
 @app.put("/user/{user_id}/{username}/{age}")
 async def change_registration(user_id: str, username: str, age: int) -> str:
     users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User {user_id} is registered"
+    return f"User {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
-async def change_registration(user_id: str) -> str:
+async def delete_registration(user_id: str) -> str:
     users.pop(user_id)
+    return f"User {user_id} has been deleted"
