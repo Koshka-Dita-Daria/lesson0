0de2b6dd591380d1a0cbecf0a7719722fbdb2387
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-10-29 14:10:56 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -21,35 +21,31 @@ async def call_dictionary() -> List[Users]:
 
 @app.post("/user/{username}/{age}")
 async def registration(username: str, age: int) -> Users:
-    if len(users[:-1]) == 0:
-        Users.id = 1
-    else:
-        Users.id = len(users[-1])+1
-    Users.age = age
-    Users.username = username
     users.append(Users)
-    try:
-        return users[Users.id-1]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Message not found")
+    if len(users) == 1:
+        users[len(users)-1].id = 1
+    else:
+        users[len(users)-1].id = users[len(users)-2].id+1
+    users[len(users)-1].username = username
+    users[len(users)-1].age = age
+    return users[len(users)-1]
 
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def change_registration(user_id: int, username: str, age: int) -> Users:
-    try:
-        Users.id = user_id
-        Users.age = age
-        Users.username = username
-        return users[user_id]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 
 @app.delete("/user/{user_id}")
 async def delete_registration(user_id: int) -> Users:
     try:
-        users.pop(user_id-1)
-        return users[user_id-1]
+        users.pop(user_id)
+        return users[user_id]
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
