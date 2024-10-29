46925e820781d0a2ea2cfbfa981fdae8819cfdd2
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-10-29 15:02:47 +0300

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -21,14 +21,13 @@ async def call_dictionary() -> List[Users]:
 
 @app.post("/user/{username}/{age}")
 async def registration(username: str, age: int) -> Users:
-    users.append(Users)
-    if len(users) == 1:
-        users[len(users)-1].id = 1
+    if len(users) == 0:
+        user_id = 1
     else:
-        users[len(users)-1].id = users[len(users)-2].id+1
-    users[len(users)-1].username = username
-    users[len(users)-1].age = age
-    return users[len(users)-1]
+        user_id = len(users)+1
+    k = Users(id=user_id, username=username, age=age)
+    users.append(k)
+    return k
 
 
 @app.put("/user/{user_id}/{username}/{age}")
@@ -44,8 +43,8 @@ async def change_registration(user_id: int, username: str, age: int) -> Users:
 
 @app.delete("/user/{user_id}")
 async def delete_registration(user_id: int) -> Users:
-    try:
-        users.pop(user_id)
-        return users[user_id]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User was not found")
+    for user in users:
+        if user.id == user_id:
+            users.pop(user_id-1)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
