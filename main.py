28486a65b24d7c043f992d78064bae5a3715dce8
Koshka-Dita-Daria <90615238+Koshka-Dita-Daria@users.noreply.github.com>
2024-11-14 14:50:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import os
-import time
-import sys
-directory = '.'
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        filepath = os.path.join(root, file)
-        filetime = os.path.getatime(filepath)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        fileize = os.path.getsize(filepath)
-        parent_dir = os.path.dirname(filepath)
-        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {fileize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+# 1st program
+print(9**0.5*5)
+# 2nd program(9.99 не может быть больше 1000)
+print((9.99 > 1.000 and 9.99 > 9.98) and 9.99 != 1000.1)
+# 3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(a)
+print(b)
+print(a == b)
+# 4 program
+a = float('123.456')
+print(int(a*10) % 10)
